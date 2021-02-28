74271065313594b5b21e5ca08a775c4c986abbd4
Alexsander <git@alextelles.dev.br>
2021-02-28 17:34:33 -0300

    Material ui Configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
-      <h1>Clone youtube com NextJS</h1>
+      Clone youtube com NextJS
+      <Button variant="outlined" color="primary">
+        Primary
+      </Button>
     </div>
   );
 }
